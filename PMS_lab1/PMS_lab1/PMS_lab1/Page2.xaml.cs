abd58458abcdf8e80d757936388d896d39c0255c
yulya3720 <yulya3720@gmail.com>
2021-05-21 13:10:53 +0300

    lab2

--- PMS_lab1/PMS_lab1/PMS_lab1/Page2.xaml.cs
+++ PMS_lab1/PMS_lab1/PMS_lab1/Page2.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microcharts;
+using PMS_lab1.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +16,45 @@ namespace PMS_lab1
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Page2 : ContentPage
     {
+
+        private static readonly PieChart pieChart = new PieChart { Entries = ChartData.PieEntries, LabelMode = LabelMode.None, };
+        private static readonly LineChart lineChart = new LineChart { Entries = ChartData.LineEntries, LineSize = 3, LineMode = LineMode.Straight, PointMode = PointMode.None };
+
+        bool switched = false;
         public Page2()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            chartView.Chart = pieChart;
+        }
+
+        void OnClick(object sender, EventArgs e)
+        {
+            if (switched)
+            {
+                chartView.Chart = pieChart;
+                chartSwitch.Text = "Замінити на графік";
+                chartPage.Title = "Діаграма";
+            }
+            else
+            {
+                chartView.Chart = lineChart;
+                chartSwitch.Text = "Замінити на діаграму";
+                chartPage.Title = "Графік";
+            }
+
+            switched = !switched;
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            chartView.HeightRequest = Math.Min(height - 50, width);
         }
     }
 }
